bb0247c616d56fc6c97ad4b869fd28bee24e8d70
dfleetwood <darren.fleetwood@madano.com>
2020-04-25 07:51:15 +0100

    run update

--- flows/data_load_flow.py
+++ flows/data_load_flow.py
@@ -7,30 +7,33 @@ import sys
 import pandas as pd
 import cloudpickle
 
+
 @task(name="Data check load", log_stdout=True)
 def data_check():
-    
+
     logger = prefect.utilities.logging.get_logger()
-    
-    with open ("../data/data_file.csv") as f:
-        data_x = pd.read_csv (f)
-    logger.debug ("Loaded data")
-    logger.debug (data_x.shape)
-    print (data_x.shape)
-    
+
+    with open("./data/data_file.csv") as f:
+        data_x = pd.read_csv(f)
+    logger.debug("Loaded data")
+    logger.debug(data_x.shape)
+    print(data_x.shape)
+
 # @task(name="Data check v2 dot", log_stdout=True)
 # def data_check_v2():
-    
+
 #     logger = prefect.utilities.logging.get_logger()
-    
+
 #     with open ("../data/raw/sysomos_heam_tweet_sample/sysomos_heam_tweet_sample-050320-1531.csv") as f:
 #         data_x = pd.read_csv (f)
 #     logger.debug ("Loaded data")
 #     logger.debug (data_x.shape)
 
- 
-flow =  Flow("data_load_test",  tasks = [data_check])
 
-with open ("./data_load_test", "wb") as f:
-    cloudpickle.dump (flow, f)
+flow = Flow("data_load_test",  tasks=[data_check])
+
+# with open("./data_load_test", "wb") as f:
+#     cloudpickle.dump(flow, f)
 #flow.save ("./data_load_test")
+
+flow.run()
