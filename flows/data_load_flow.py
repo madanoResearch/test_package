d99c7f816dec937e4d229eabc05dea1c504ef76c
dfleetwood <darren.fleetwood@madano.com>
2020-04-26 15:36:17 +0100

    run

--- flows/data_load_flow.py
+++ flows/data_load_flow.py
@@ -1,4 +1,5 @@
 import prefect
+#import prefect.context
 from prefect import task, Flow, Parameter
 from prefect.environments.storage import Docker
 import os
@@ -6,10 +7,58 @@ import subprocess
 import sys
 import pandas as pd
 import cloudpickle
+import functools
+
+#import sys
+#sys.path.append("C:/Users/dfleetwood/Documents/prefect_deployment")
+
+#import prefect_deployment
+
+# def flow_email_state_handler(obj, old_state, new_state):
+#     # print(new_state)
+#     # try:
+
+#     tasks = obj.tasks  # prefect.context.to_dict() #.get("date")
+#     for task in tasks:
+#         if isinstance(task, Parameter) & (task.name == 'email_address'):
+#            print ("Found it ")
+#            email_address = task
+#     try:
+#         email_address  =  str (email_address.run())
+#     except Exception as e:
+#         print (e)
+        
+
+    # get(
+    #     # "parameters", {}).get("to_email_address")
+    #     print("Params")
+    #     print(email_address)
+    #     if email_address is not None:
+    #         print("Email address: " + str(email_address))
+    #         send_email(email_address, "Prefect flow: " +
+    #                    str(obj.name), str(new_state))
+    #         print("Sent email")
+    #     else:
+    #         print("None email adrress")
+    # except Exception as e:
+    #     print(e)
+
+    #return new_state
+
+
+# @task
+# def setup(email_address):
+#     prefect.context['email_address'] = email_address
 
 
 @task(name="Data check load", log_stdout=True)
-def data_check(param_t):
+def data_check():
+    # to_email_address = prefect.context.to_dict()#  parameters #prefect.context.get("parameters", {})  # .get("to_email_address")
+    # print("Params: ")  # + to_email_address)
+    # print(type (to_email_address))
+    # print(str (to_email_address))
+    # p = prefect.context.get("parameters", {})#.get("to_email_address")
+    # print ("Flow name" + str (p))  #str (prefect.context.parameters))
 
     logger = prefect.utilities.logging.get_logger()
 
@@ -18,7 +67,7 @@ def data_check(param_t):
     logger.debug("Loaded data")
     logger.debug(data_x.shape)
     print(data_x.shape)
-    print(param_t)
+    # print(param_t)
 
 # @task(name="Data check v2 dot", log_stdout=True)
 # def data_check_v2():
@@ -32,16 +81,26 @@ def data_check(param_t):
 
 
 with Flow("data_load_test") as flow:
-    test_param = Parameter("test_param", default="a")
-    data_check(test_param)
+    #test_param = Parameter("test_param", default="a")
+    flow.add_task(data_check)
 
     # flow.add_task(data_check)
     #flow.add_task(Parameter("test_param", default="a"))
 
-#flow = Flow("data_load_test",  tasks=[data_check])
+    #flow = Flow("data_load_test",  tasks=[data_check])
 
-# with open("./data_load_test", "wb") as f:
-#     cloudpickle.dump(flow, f)
-#flow.save ("./data_load_test")
+    # with open("./data_load_test", "wb") as f:
+    #     cloudpickle.dump(flow, f)
+    #flow.save ("./data_load_test")
+ #   flow.add_task(Parameter("email_address",
+ #                             default="darrenfleetwood@gmail.com"))
+    #data_check.set_upstream(setup(email_address))
 
-# flow.run()
+    #flow.add_task(Parameter("to_email_address", to_email_address))
+    #environment_setup.set_upstream(task, flow=flow)
+
+    # flow.state_handlers.append(flow_email_state_handler)
+#flow.state_handlers.append(functools.partial (prefect_deployment.deploy.flow_email_state_handler, email_address = email_address))
+#flow.state_handlers.append(prefect_deployment.deploy.flow_email_state_handler)
+
+#flow.run()
